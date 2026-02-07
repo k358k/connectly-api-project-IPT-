654a49121c44ab01e18c119d2bd22949e1e5250a
DullasZLS <lr.zldullas@mmdc.mcl.edu.ph>
2026-02-07 21:03:18 +0800

    Add files via upload

---
+++ posts/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+# Defines what User is 
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True) 
+    email = models.EmailField(unique=True) 
+    created_at = models.DateTimeField(auto_now_add=True)  
+
+
+    def __str__(self):
+        return self.username
+
+
+
+# Defines what Post is and assigns user who created the post
+class Post(models.Model):
+    content = models.TextField() 
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE) 
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.created_at}"
+
+
+
+# Defines what Comment is and assigns its author and parent post
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on Post {self.post.id}"
+
---
+++ posts/views.py
@@ -0,0 +1,54 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .models import Post
+from .models import Comment
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
+
+# Defines and validates User creation and retrieval
+class UserListCreate(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Defines and validates Post creation and retrieval
+class PostListCreate(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Defines and validates Comment creation and retrieval
+class CommentListCreate(APIView):
+    def get(self, request):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
